f244c8a81a4a15605f8e0cf7c93749dc61e87322
github username <angelina.w25@gmail.com>
2019-01-16 12:11:21 -0500

    started site, changed some infomation.

---
+++ portfolio-site/portfolio-v1/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Footer from './Footer'
+import avatar from '../assets/images/avatar.jpg'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header id="header">
+                <div className="inner">
+                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
+                    <h1><strong>I am Angelina</strong>, a Full Stack Web Developer. <br />
+                    Welcome to my project site!</h1>
+                </div>
+                <Footer />
+            </header>
+        )
+    }
+}
+
+export default Header
