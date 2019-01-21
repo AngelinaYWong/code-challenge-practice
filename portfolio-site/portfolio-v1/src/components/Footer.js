f383a47edafa2daa05d02f08716ffc7dbddffab7
github username <angelina.w25@gmail.com>
2019-01-21 17:11:02 -0500

    find medium icon

--- portfolio-site/portfolio-v1/src/components/Footer.js
+++ portfolio-site/portfolio-v1/src/components/Footer.js
@@ -8,7 +8,8 @@ class Footer extends React.Component {
                     <ul className="icons">
                         <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
                         <li><a href="https://github.com/AngelinaYWong" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
+                        <li><a href="https://medium.com/@angelina.wong" className="icon fa-dribbble"><span className="label">Medium</span></a></li>
+                        // find medium or blog icon
                         <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
